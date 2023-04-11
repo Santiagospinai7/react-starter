b5e11849f57441f2f2df7022b55aeb5752b03bc5
Santiago Ospina Idrobo <santiagospidrobo15@gmail.com>
2023-04-11 14:15:10 +0200

    add likes counter and save likes inside the TweetsList

--- src/components/CreateTweet.js
+++ src/components/CreateTweet.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
 
 const CreateTweet = ({tweets, setTweets, textInput, setTextInput}) => {
-
   // Functions
   const userInputHandler = (e) => {
     setTextInput(e.target.value)
@@ -10,7 +10,7 @@ const CreateTweet = ({tweets, setTweets, textInput, setTextInput}) => {
   const submitTweetHandler = (e) => {
     e.preventDefault();
     //set the tweets is like a push to a tweets array
-    setTweets([...tweets, textInput]);
+    setTweets([...tweets, {message: textInput, id: uuidv4(), likes: 0}]);
     setTextInput("");
   }
 
@@ -18,7 +18,6 @@ const CreateTweet = ({tweets, setTweets, textInput, setTextInput}) => {
     <form onSubmit={submitTweetHandler}>
       <textarea value={ textInput } onChange={ userInputHandler } cols="30" rows="10"></textarea>
       <button>Submit</button>
-      
       {/* Here see the tweet text and add click event that delete the text */}
       {/* <h1 onClick={ () => setTextInput('') }>{ textInput }</h1> */}
     </form>
--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,20 +1,32 @@
-import React from "react";
+import React, { useState } from 'react';
 
 const Tweet = ({name, tweet, tweets, setTweets}) => {
+  // State
+  const [tweetLike, setTweetLike] = useState(0);
+
   const deleteTweet = () => {
-    setTweets(tweets.filter(t => t !== tweet ));
+    setTweets(tweets.filter(state => state.id !== tweet.id ));
   }
 
   const like = () => {
-    console.log('like');
+    //setTweetLike
+    let likes = tweetLike + 1;
+    setTweetLike(likes);
+
+    //modify likes in the setTweets list
+    const tweetIndex = tweets.findIndex((element) => element.id === tweet.id)
+
+    tweets[tweetIndex].likes = likes;
+    setTweets(tweets);
   }
 
   return (
     <div>
       <h2>Name {name}</h2>
-      <h3>{tweet}</h3>
+      <h3>{tweet.message}</h3>
       <button onClick={deleteTweet}>Delete</button>
       <button onClick={like}>Like</button>
+      <p><strong>Likes:</strong> { tweetLike } </p>
     </div>
   );
 }
